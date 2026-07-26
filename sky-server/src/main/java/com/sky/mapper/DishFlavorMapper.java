bc5095d73950474beb66de875a382de85847366f
cn.WuYu_ <wuyu2440933838@gmail.com>
2026-07-26 15:24:26 +0800

    功能-新增菜品功能完成

---
+++ sky-server/src/main/java/com/sky/mapper/DishFlavorMapper.java
@@ -0,0 +1,12 @@
+package com.sky.mapper;
+
+import com.sky.entity.DishFlavor;
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+
+import java.util.List;
+
+@Mapper
+public interface DishFlavorMapper {
+    void insertFlavors(@Param("flavors") List<DishFlavor> flavors);
+}
